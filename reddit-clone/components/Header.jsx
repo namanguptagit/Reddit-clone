fdf0651340a5ca16054075d2b0fb787aeb532d9f
Naman Gupta <naman312000@gmail.com>
2025-05-29 18:29:22 +0530

    Working Implemented clerk based authentication
    Working Implemented clerk based authentication

---
+++ reddit-clone/components/Header.jsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { SignedIn,SignedOut,SignInButton,UserButton } from "@clerk/nextjs";
+import { Button } from "./ui/button";
+import reddishLogoOnly from "@/images/Reddish Logo Only.png";
+import reddishLogo from "@/images/Reddish Full.png";
+import Image from "next/image";
+import { ChevronLeftIcon, MenuIcon } from "lucide-react";
+import { useSidebar } from "@/components/ui/sidebar";
+
+const Header = () => {
+  const { isMobile, toggleSidebar , open } = useSidebar();
+  return (
+    <header className="flex justify-between items-center p-4 border-b border-gray-200">
+      <div className="flex items-center gap-2">
+        {open && !isMobile ? (<ChevronLeftIcon className="w-6 h-6" onClick={toggleSidebar} />): (
+          <div className="flex items-center gap-2">
+        <MenuIcon className="w-6 h-6" onClick={toggleSidebar} />
+        <Image src={reddishLogo} alt="Reddish Logo" width={150} height={150} className="hidden md:block" />
+        <Image src={reddishLogoOnly} alt="Reddish Logo" width={40} height={40} className="block md:hidden" />
+      </div>
+        )}
+        </div>
+      <div>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <SignedOut>
+          <Button asChild variant="outline">
+            <SignInButton mode="modal" />
+          </Button>
+        </SignedOut>
+      </div>
+    </header>
+  );
+}
+
+export default Header
---
+++ reddit-clone/components/app-sidebar.tsx
@@ -0,0 +1,133 @@
+import * as React from "react"
+import { FlameIcon, HomeIcon, Minus, Plus, TrendingUpIcon } from "lucide-react"
+import Link from "next/link";
+import { SearchForm } from "@/components/search-form"
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+  SidebarRail,
+} from "@/components/ui/sidebar"
+import Image from "next/image";
+import reddishLogo from "@/images/Reddish Full.png";
+// This is sample data.
+type SidebarData = {
+  navMain: {
+    title: string;
+    url: string;
+    items: { title: string; url: string; isActive: boolean }[];
+  }[];
+};
+const data: SidebarData = {
+  navMain: [
+    {
+      title: "Communities",
+      url: "#",
+      items: [
+        {
+          title: "Installation",
+          url: "#",
+          isActive: false,
+        },
+        {
+          title: "Project Structure",
+          url: "#",
+          isActive: false,
+        },
+      ],
+    },
+  ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <Link href="/">
+                <Image src={reddishLogo} alt="Reddish Logo" width={150} height={150} />
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+        <SearchForm />
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton asChild></SidebarMenuButton>
+              <SidebarMenuButton asChild className="p-5">
+                <Link href="/">
+                  <HomeIcon className="w-4 h-4 mr-2" />Home
+                </Link>
+              </SidebarMenuButton>
+              <SidebarMenuButton asChild className="p-5">
+                <Link href="/popular">
+                  <TrendingUpIcon className="w-4 h-4 mr-2" />Popular
+                </Link>
+              </SidebarMenuButton>
+              <SidebarMenuButton asChild className="p-5">
+                <Link href="/hot">
+                  <FlameIcon className="w-4 h-4 mr-2" />Hot/Controversial
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroup>
+        <SidebarGroup>
+          <SidebarMenu>
+            {data.navMain.map((item, index) => (
+              <Collapsible
+                key={item.title}
+                defaultOpen={index === 1}
+                className="group/collapsible"
+              >
+                <SidebarMenuItem>
+                  <CollapsibleTrigger asChild>
+                    <SidebarMenuButton>
+                      {item.title}{" "}
+                      <Plus className="ml-auto group-data-[state=open]/collapsible:hidden" />
+                      <Minus className="ml-auto group-data-[state=closed]/collapsible:hidden" />
+                    </SidebarMenuButton>
+                  </CollapsibleTrigger>
+                  {item.items?.length ? (
+                    <CollapsibleContent>
+                      <SidebarMenuSub>
+                        {item.items.map((item) => (
+                          <SidebarMenuSubItem key={item.title}>
+                            <SidebarMenuSubButton
+                              asChild
+                              isActive={item.isActive}
+                            >
+                              <a href={item.url}>{item.title}</a>
+                            </SidebarMenuSubButton>
+                          </SidebarMenuSubItem>
+                        ))}
+                      </SidebarMenuSub>
+                    </CollapsibleContent>
+                  ) : null}
+                </SidebarMenuItem>
+              </Collapsible>
+            ))}
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
